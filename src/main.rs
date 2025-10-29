5508124c9ea4ebec3d6ade9187a567a8f3b39649
Pares <alfariz2004@gmail.com>
2025-10-30 01:10:36 +0700

    feat: done register endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use axum::{
     routing::{get, post},
     response::Json,
-    Router
+    Router, debug_handler
 };
 
-use serde::Serialize;
-use sqlx::{SqlitePool, FromRow};
+use serde::{Deserialize, Serialize};
+use sqlx::{SqlitePool, FromRow, Pool, Executor, query};
 use std::net::SocketAddr;
-
+use axum::extract::State;
+use axum::http::StatusCode;
+use sqlx::sqlite::SqliteQueryResult;
+use bcrypt::{hash, DEFAULT_COST};
 
 #[derive(Serialize, FromRow)]
 struct Schedule {
@@ -27,14 +30,57 @@ async fn view_schedule(pool: axum::extract::State<SqlitePool>) -> Json<Vec<Sched
     Json(schedules)
 }
 
+
+#[derive(Serialize)]
+struct UserResponse{
+    message: String,
+}
+
+#[derive(Deserialize)]
+struct RegisterRequest{
+    username: String,
+    password: String,
+}
+
+#[debug_handler]
+async fn register_user(
+    State(pool): State<SqlitePool>,
+    Json(payload): Json<RegisterRequest>
+) -> Json<UserResponse> {
+
+    let hashed = hash(&payload.password, DEFAULT_COST).unwrap();
+
+    let result = sqlx::query!(
+        "INSERT INTO user(username, password) VALUES (?1, ?2)",
+        payload.username,
+        hashed
+    )
+    .execute(&pool)
+    .await;
+
+    match result {
+        Ok(_) => Json(UserResponse {
+            message: "Successfully registered user".to_string(),
+        }),
+        Err(e) => Json(UserResponse {
+            message: format!("Failed to register user: {}", e),
+        }),
+    }
+}
+
+
 #[tokio::main]
 async fn main() {
+
+    dotenvy::dotenv().ok();
+
     let pool = SqlitePool::connect("sqlite:trans_banyumas.db")
         .await
         .expect("Failed to create Sqlite database pool");
 
     let app = Router::new()
         .route("/jadwal", get(view_schedule))
+        .route("/register", post(register_user))
         .with_state(pool);
 
     println!("Running server on http://localhost:3000");
